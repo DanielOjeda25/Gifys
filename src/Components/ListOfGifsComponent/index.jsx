f40ad3506c69a8f83bcd6d3a8668841970f88508
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2022-05-25 21:45:15 -0300

    component list logic

---
+++ src/Components/ListOfGifsComponent/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Gifs from "../GifComponent";
+import { Grid } from "@chakra-ui/react";
+import "./styles.css";
+
+export default function ListofGifs({ gifs }) {
+  return (
+    <Grid
+      minH="100vh"
+      alignItems="center"
+      gap="6px 6px"
+      gridGap="6px 6px"
+      gridAutoRows='250px'
+      gridAutoFlow= 'row dense'
+      className="ListOfGifs"
+    >
+      {gifs.map(({ id, title, url }) => (
+        <Gifs key={id} title={title} url={url} id={id} />
+      ))}
+    </Grid>
+  );
+}
