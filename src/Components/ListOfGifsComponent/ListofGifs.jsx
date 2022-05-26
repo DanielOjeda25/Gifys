0a562c5ce757357de06c18baeca6b1bfc514c254
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-05-25 22:05:52 -0300

    Delete ListofGifs.jsx

--- src/Components/ListOfGifsComponent/ListofGifs.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Gifs from "../GifComponent/Gifs";
-
-
-export default function ListofGifs({ gifs }) {
-  return gifs.map((gif) => (
-    <Gifs title={gif.title} url={gif.url} key={gif.id} />
-  ));
-}
