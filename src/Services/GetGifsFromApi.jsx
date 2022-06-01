3f817cac2479b7d4729dadc7cd788b10a1614e0d
Daniel Ojeda <ojedadanielalejandro333@gmail.com>
2022-06-01 13:53:54 -0300

    se corrigieron algunos errores

--- src/Services/GetGifsFromApi.jsx
+++ src/Services/GetGifsFromApi.jsx
@@ -1,23 +1,23 @@
-import { API_URL, API_KEY } from "./settings";
+import { API_URL, API_KEY } from './settings'
 
 const getGifsFromApi = (response) => {
-  const { data = [] } = response;
+  const { data = [] } = response
   if (Array.isArray(data)) {
-    //we get the gifs from the api
+    // we get the gifs from the api
     const gifs = data.map((image) => {
-      const { images, title, id } = image;
-      const { url } = images.downsized;
-      return { title, id, url };
-    });
-    return gifs;
+      const { images, title, id } = image
+      const { url } = images.downsized
+      return { title, id, url }
+    })
+    return gifs
   }
-  return [];
-};
+  return []
+}
 
-export default function GetGifsFromApi({limit = 25} = {}) {
-  const apiURL = `${API_URL}/gifs/search?${API_KEY}&q=vaporwave&limit=${limit}&offset=0&rating=g&lang=en`;
+export default function GetGifsFromApi ({ limit = 25 } = {}) {
+  const apiURL = `${API_URL}/gifs/search?${API_KEY}&q=vaporwave&limit=${limit}&offset=0&rating=g&lang=en`
 
   return fetch(apiURL)
     .then((res) => res.json())
-    .then(getGifsFromApi);
+    .then(getGifsFromApi)
 }
