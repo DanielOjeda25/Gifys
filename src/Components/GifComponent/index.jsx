4dfc3c918412332b5ba3f55a363c2e781238cabd
Daniel Ojeda <ojedadanielalejandro333@gmail.com>
2022-06-06 12:06:42 -0300

    se agregaron los estilos de chakra ui

--- src/Components/GifComponent/index.jsx
+++ src/Components/GifComponent/index.jsx
@@ -1,12 +1,31 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import './styles.css'
 
 export default function Gifs ({ title, url }) {
   return (
-    <Box position="relative" className="Gif">
-      <h3>{title}</h3>
-      <img src={url} alt={title} className="img-gif" />
+    <Box position='relative' h='100%' className='Gif'>
+      <Text
+        className='text-gifs'
+        borderRadius='5px'
+        fontSize='xs'
+        margin={0}
+        position='absolute'
+        p='.5em'
+        color='white'
+        bottom='0'
+        bg='rgba(0, 0, 0, 0.1)'>
+        {title}
+      </Text>
+      <Image
+        src={url}
+        alt={title}
+        className='img-gif'
+        h='100%'
+        w='100%'
+        objectFit='contain'
+        verticalAlign='top'
+      />
     </Box>
   )
 }
