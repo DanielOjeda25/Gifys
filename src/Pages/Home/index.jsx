e0f73e1b75b0a29110685cd864e452768a62d81f
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-06-21 13:23:01 -0300

    homepage end and styles

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './index.css'
+import { Helmet } from 'react-helmet'
+
+import { Box, Text } from '@chakra-ui/react'
+
+import { useGifs } from '../../Hooks/useGifs'
+import SearchForm from '../../Components/SearchFormComponent/index'
+import ListofGifs from '../../Components/ListOfGifsComponent/index'
+import TrendingSearches from '../../Components/TrendingSearches'
+
+export default function Home () {
+  const { gifs } = useGifs()
+
+  return (
+    <>
+      <Helmet>
+        <title>Home | Giffy</title>
+      </Helmet>
+      <Box
+        bg={'inherit'}
+        position={'stycky'}
+        top={0}
+        zIndex={20}
+        maxH={'3rem'}
+      >
+        <SearchForm />
+      </Box>
+      <Box
+        w={'100%'}
+        maxW={'90rem'}
+        m={'0 auto'}
+        p={'0 0.5em'}
+      >
+        <Box
+          display={'flex'}
+          flexDir={'column'}
+          alignItems={'flex-start'}
+        >
+          <Box
+            flexShrink={2}
+            w={'100%'}
+          >
+            <Text
+              color={'white'}
+              marginTop={'0.6rem'}>
+              Ultima Busqueda
+            </Text>
+            <ListofGifs gifs={gifs} />
+          </Box>
+          <Box
+            marginBottom={'25px'}
+            w={'100%'}
+            className='trending-searches'
+          >
+            <TrendingSearches />
+          </Box>
+        </Box>
+      </Box>
+    </>
+  )
+}
