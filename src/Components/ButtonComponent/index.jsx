b6b9f7a451e717e1e9155bfcecb57c32162c9b56
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-06-21 13:25:30 -0300

    estilos al botton

--- src/Components/ButtonComponent/index.jsx
+++ src/Components/ButtonComponent/index.jsx
@@ -5,11 +5,20 @@ import { Link } from 'wouter'
 export default function ButtonComponent ({ children, href, size }) {
   return href
     ? (
-    <Link size={size} href={href}>
-      {children}
-    </Link>
+      <Link size={size} href={href}>
+        {children}
+      </Link>
       )
     : (
-    <Button size={size}>{children}</Button>
+      <Button
+        size={size}
+        bg={'white'}
+        border={'1px solid transparent'}
+        color={'black'}
+        cursor={'pointer'}
+        fontSize={'1.2rem'}
+        p={'.5rem 1rem'}>
+        {children}
+      </Button>
       )
 }
