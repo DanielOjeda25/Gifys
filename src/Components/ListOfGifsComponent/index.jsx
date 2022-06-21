61bfbeb6335188064c902a20d3efbebe3eeae7f0
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-06-21 13:25:43 -0300

    "

--- src/Components/ListOfGifsComponent/index.jsx
+++ src/Components/ListOfGifsComponent/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import Gifs from "../GifComponent";
-import { Grid } from "@chakra-ui/react";
-import "./styles.css";
+import React from 'react'
+import Gifs from '../GifComponent'
+import { Grid } from '@chakra-ui/react'
+import './styles.css'
 
-export default function ListofGifs({ gifs }) {
+export default function ListofGifs ({ gifs }) {
   return (
     <Grid
       minH="100vh"
@@ -18,5 +18,5 @@ export default function ListofGifs({ gifs }) {
         <Gifs key={id} title={title} url={url} id={id} />
       ))}
     </Grid>
-  );
+  )
 }
