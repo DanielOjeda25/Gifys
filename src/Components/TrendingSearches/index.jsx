1fc2e1832866eb25f88f493547406375376a73c3
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-06-21 13:26:43 -0300

    trending searches update

---
+++ src/Components/TrendingSearches/index.jsx
@@ -0,0 +1,19 @@
+import React, { Suspense } from 'react'
+
+import useNearScreen from '../../Hooks/useNearScreen'
+import { Spinner } from '@chakra-ui/react'
+
+const TrendingSearches = React.lazy(() => import('./TrendingSearches'))
+
+export default function LazyTrending () {
+  const { isNearScreen, fromRef } = useNearScreen({
+    distance: '0px'
+  })
+  return (
+    <div ref={fromRef}>
+      <Suspense fallback={<Spinner color={'white'} size={'lg'} />}>
+        {isNearScreen ? < TrendingSearches /> : <Spinner color={'white'} size={'lg'} />}
+      </Suspense>
+    </div>
+  )
+}
