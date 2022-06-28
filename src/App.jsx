8c1c793eb4b4b3d342563ecf2ab88da712f4a420
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-06-27 22:38:26 -0300

    cambios terminados

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
-
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { Suspense } from 'react'
 import './App.css'
 
-import GetGifsFromApi from './Services/GetGifsFromApi'
-import ListofGifs from './Components/ListOfGifsComponent'
+import { GifsContext } from './Context/GifsContext'
 
 import { Box, Image } from '@chakra-ui/react'
-import { Link } from 'wouter'
+import { Link, Route, Switch } from 'wouter'
 
 const HomePage = React.lazy(() => import('./Pages/Home/index'))
 
 function App () {
-  const [gifs, setGifs] = useState([])
-
-  useEffect(() => {
-    GetGifsFromApi().then((gifs) => setGifs(gifs))
-  })
   return (
-    <Box>
-      <Suspense>
-        <section>
+    <Box
+      textAlign={'center'}
+    >
+      <Suspense fallback={null}>
+        <Box
+          bg='blackAlpha.800'
+          color='White'
+          flexDir='column'
+          fontSize='calc(10px + 2vmin)'
+          minHeight='100vh'
+          p='16px'
+          textAlign='left'
+        >
           <Link to='/'>
-            <figure>
-              <Image alt='giffys logo' src='../src/Images/react.svg'/>
-            </figure>
+            <Box
+              m='10px auto'
+              p='20px 0'
+              w='144px'>
+              <Image alt='giffys logo' src='../src/Images/react.svg'
+                w='100%'
+                objectFit='cover'
+              />
+            </Box>
           </Link>
-          <ListofGifs gifs={gifs} />
-        </section>
+          <GifsContext>
+            <Switch>
+              <Route component={HomePage} path='/' />
+            </Switch>
+          </GifsContext>
+        </Box>
       </Suspense>
     </Box>
   )
