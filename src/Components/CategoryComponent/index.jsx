e1afb3221565ce7720ba6e523e528d2331d1949f
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-06-28 19:36:11 -0300

    conversion del archivo

--- src/Components/CategoryComponent/index.jsx
+++ src/Components/CategoryComponent/index.jsx
@@ -1,26 +1,41 @@
-import { Box, ListItem, Text, UnorderedList } from '@chakra-ui/react'
 import React from 'react'
-import { Link } from 'wouter'
 import './styles.css'
+
+import { Box, ListItem, Text, UnorderedList } from '@chakra-ui/react'
+import { Link } from 'wouter'
 
 export default function Category ({ name, options = [] }) {
   return (
-    <Box className='Category'>
-      <Text fontSize='xl' color='white'>
-        {name}
-      </Text>
+    <Box>
+      <Text
+        color='White'
+        textAlign={'center'}
+        marginBottom={'0.7rem'}
+        marginTop={'0.6rem'}
+        className='Title'
+      >{name}</Text>
       <UnorderedList
-        stylePosition='inside'
-        display='flex'
-        flexWrap='wrap'
-        justifyContent='space-evenly'>
-        {options.map((op) => (
-          <ListItem
-            key={op}
-            color='white'
-            fontSize='1em'
-            transition='color ease-in 0.1s'>
-            <Link to={`/search/${op}`}></Link>
+        listStylePosition={'inside'}
+        display={'flex'}
+        flexWrap={'wrap'}
+        padding={'0'}
+        margin={'0'}
+        justifyContent={'space-around'}
+        className='List'
+      >
+        {options.map((singleOption, index) => (
+          <ListItem key={singleOption} index={index} type="primary"
+            padding={'0.3rem'}
+            margin={'0.2rem'}
+            fontSize={'1.2rem'}
+            borderRadius={'0.3rem'}
+            className='ListItem color-change-2x'
+          >
+            <Link to={`/search/${singleOption}`}
+              className='Link'
+            >
+              {singleOption}
+            </Link>
           </ListItem>
         ))}
       </UnorderedList>
