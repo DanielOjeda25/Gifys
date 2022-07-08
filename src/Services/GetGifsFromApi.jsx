42c3d2a6ab478c660b62cabe04ff0c0b11dec58d
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-07-08 13:41:18 -0300

    reversion de cambios

--- src/Services/GetGifsFromApi.jsx
+++ src/Services/GetGifsFromApi.jsx
@@ -4,17 +4,18 @@ const fromApiResponseToGifs = (response) => {
   const { data = [] } = response
   if (Array.isArray(data)) {
     // we get the gifs from the api
-    return data.map((image) => {
+    const gifs = data.map((image) => {
       const { images, title, id } = image
       const { url } = images.downsized
       return { title, id, url }
     })
+    return gifs
   }
   return []
 }
 
-export default function GetGifsFromApi ({ limit = 25 } = {}) {
-  const apiURL = `${API_URL}/gifs/search?${API_KEY}&q=programmer&limit=${limit}&offset=0&rating=g&lang=en`
+export default function GetGifsFromApi ({ limit = 15, keyword = 'memes' } = {}) {
+  const apiURL = `${API_URL}/gifs/search?${API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`
 
   return fetch(apiURL)
     .then((res) => res.json())
