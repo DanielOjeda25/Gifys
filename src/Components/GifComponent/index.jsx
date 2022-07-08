aef9b1ee41d82e8b1d49473594c81a06a56d7333
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-07-08 13:43:47 -0300

    component terminado

--- src/Components/GifComponent/index.jsx
+++ src/Components/GifComponent/index.jsx
@@ -1,32 +1,36 @@
 import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'wouter'
 import './styles.css'
 
-export default function Gifs ({ title, url }) {
+export default function Gifs ({ title, url, id }) {
   // arreglar las letras, escalable y cambiar el grid
   return (
     <Box position='relative' h='100%' className='Gif'>
-      <Text
-        borderRadius='5px'
-        fontSize='.45em'
-        margin={0}
-        position='absolute'
-        p='.5em'
-        color='white'
-        bottom='0'
-        bg='rgba(0, 0, 0, 0.1)'
-        className='text-gifs'>
-        {title}
-      </Text>
-      <Image
-        src={url}
-        alt={title}
-        className='img-gif'
-        h='100%'
-        w='100%'
-        objectFit='contain'
-        verticalAlign='top'
-      />
+      <Link to={`/gif/${id}`}>
+        <Text
+          borderRadius='5px'
+          fontSize='.45em'
+          margin={0}
+          position='absolute'
+          p='.5em'
+          color='white'
+          bottom='0'
+          bg='rgba(0, 0, 0, 0.1)'
+          className='text-gifs'>
+          {title}
+        </Text>
+        <Image
+          loading='lazy'
+          src={url}
+          alt={title}
+          className='img-gif'
+          h='100%'
+          w='100%'
+          objectFit='contain'
+          verticalAlign='top'
+        />
+      </Link>
     </Box>
   )
 }
